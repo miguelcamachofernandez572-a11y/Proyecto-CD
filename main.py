ab95e378dc35f80671b81e7f0b3b0a2821893855
miguelcamachofernandez572-a11y <miguelcamachofernandez572@gmail.com>
2025-12-13 17:52:03 -0500

    main actualización.1

--- main.py
+++ main.py
@@ -1,27 +1,47 @@
-import json
 import os
+import json
+import matplotlib.pyplot as plt
 
-def cargar_json(ruta_archivo):
-    
-    with open(ruta_archivo, 'r', encoding='utf-8') as f:
-        return json.load(f)
+archivos = os.listdir("json/")
 
-def promedio_precios(lista_rutas):
-    
+
+
+def promedio(producto_buscado):
     precios = []
-    for ruta in lista_rutas:
-        datos = cargar_json(ruta)
+    for archivo in archivos:
+        with open(f"json/{archivo}",encoding="utf-8") as file:
+            data = json.load(file)
+            for producto in data["productos"]:
+                if producto["nombre"] == producto_buscado and producto.get("disponible", False):
+                    precios.append(producto["precio"])
+    
+    promedio = round(sum(precios)/len(precios))
+    return promedio
         
-        for producto in datos["productos"]:
-            if producto.get("disponible", False):  
-                precios.append(producto["precio"])
-    if len(precios) == 0:
-        return 0
-    return sum(precios) / len(precios)
+diccionario = {
+    "Pan": promedio("Pan"),
+    "Leche": promedio("Leche"),
+    "Toallitas húmedas":promedio("Toallitas húmedas"),
+    "Pañales":promedio("Pañales"),
+    "yogurt":promedio("yogurt"),
+    "pollo":promedio("pollo"),
+    "picadillo":promedio("picadillo"),
+    "huevo":promedio("huevo"),
+    "queso":promedio("queso"),
+    "sopas":promedio("sopas"),
+    "arroz":promedio("arroz"),
+    "frijoles":promedio("frijoles"),
+    "aceite":promedio("aceite"),
+    "cereales":promedio("cereales"),
+    "galletas":promedio("galletas")
+}
 
-# --- Ejemplo de uso ---
-carpeta = r"C:\miguel\Proyecto CD\json"
-archivos = [os.path.join(carpeta, f"mipyme{i}.json") for i in range(1, 26)]
+def grafico_precio_promedio_vs_pension_minima(diccionario):
+    keys = diccionario.keys()
+    values = diccionario.values()
 
-promedio = promedio_precios(archivos)
-print("El precio promedio de los productos disponibles es:", promedio)
+    plt.bar(keys,values,color="#6a4d57")
+    plt.axhline(y=3056,color="#9c9386",ls="--")
+    plt.title("Precio promedio de productos")
+    plt.annotate("Pensión Mínima: 3056",(1,3000),(0,2800),arrowprops={"arrowstyle":"->"})
+    plt.show()
